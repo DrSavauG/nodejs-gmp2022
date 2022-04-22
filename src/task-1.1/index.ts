ad172f78e90ebc4a10fc330d81af875dcac8280a
DrSavauG <46158444+DrSavauG@users.noreply.github.com>
2022-04-22 14:08:57 +0300

    feat: done task-1.1

---
+++ src/task-1.1/index.ts
@@ -0,0 +1,9 @@
+import process from 'process';
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+    const input:string  = process.stdin.read();
+    const output:string  = input.split('').reverse().join('');
+    process.stdout.write(output);
+    process.stdout.write('\n');
+});
